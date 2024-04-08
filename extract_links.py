d9aedf2f367a269902c5e0731859bddbbed58d7e
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-04-08 16:13:31 +0530

    Update extract_links.py

--- extract_links.py
+++ extract_links.py
@@ -1,12 +1,1 @@
-from googlesearch import search
 
-def google_search(query, num_results=5):
-    try:
-        # Perform a Google search and print the results
-        for result in search(query, num_results=num_results):
-            print(result)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Replace 'YourQuery' with the actual query you want to search for
-google_search('django')
